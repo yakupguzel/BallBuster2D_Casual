67bb82e8c3cedc479ecf302c3ea6ee3fb2c80420
yakupguzel <33760955+yakupguzel@users.noreply.github.com>
2022-06-07 23:48:34 +0300

    Created Ball core mechanic based on numbers

--- BallBuster2D/Assets/Scripts/Ball.cs
+++ BallBuster2D/Assets/Scripts/Ball.cs
@@ -11,21 +11,74 @@ public class Ball : MonoBehaviour
     [SerializeField] ParticleSystem mergeEffect;
     [SerializeField] SpriteRenderer spriteRenderer;
 
+    bool primery;
+
     private void Start()
     {
 
         numberText.text = number.ToString();
     }
 
+    void SetBallState()
+    {
+        primery = true;
+    }
+
+    public void ChangePrimeryState()
+    {
+        Invoke("SetBallState", 2f);
+    }
+
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag(number.ToString()))
+        if (collision.gameObject.CompareTag(number.ToString()) && primery)
         {
             mergeEffect.Play();
             collision.gameObject.SetActive(false);
             number += number;
             gameObject.tag = number.ToString();
             numberText.text = number.ToString();
+
+            //Switching ball sprite
+
+            switch (number)
+            {
+                case 2:
+                    spriteRenderer.sprite = gameManager.ballSpritesList[0];
+                    break;
+                case 4:
+                    spriteRenderer.sprite = gameManager.ballSpritesList[1];
+                    break;
+                case 8:
+                    spriteRenderer.sprite = gameManager.ballSpritesList[2];
+                    break;
+                case 16:
+                    spriteRenderer.sprite = gameManager.ballSpritesList[3];
+                    break;
+                case 32:
+                    spriteRenderer.sprite = gameManager.ballSpritesList[4];
+                    break;
+                case 64:
+                    spriteRenderer.sprite = gameManager.ballSpritesList[5];
+                    break;
+                case 128:
+                    spriteRenderer.sprite = gameManager.ballSpritesList[6];
+                    break;
+                case 256:
+                    spriteRenderer.sprite = gameManager.ballSpritesList[7];
+                    break;
+                case 512:
+                case 1024:
+                case 2048:
+                    spriteRenderer.sprite = gameManager.ballSpritesList[8];
+                    break;
+                default:
+                    break;
+            }
+
+            primery = false;
+
+            ChangePrimeryState();
         }
     }
 }
--- BallBuster2D/Assets/Scripts/GameManager.cs
+++ BallBuster2D/Assets/Scripts/GameManager.cs
@@ -1,18 +1,121 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Level Settings")]
+    public Sprite[] ballSpritesList;
+    [SerializeField] private GameObject[] balls;
+    [SerializeField] private TextMeshProUGUI remainingBallText;
+
+    int remainingBallCount;
+    int ballPoolIndex;
+
+    [Header("Ball Throw Mechanism")]
+    [SerializeField] private GameObject ballThrower;
+    [SerializeField] private GameObject ballSpawnPosition;
+    [SerializeField] private GameObject nextBallPosition;
+    GameObject chosenBall;
+
+    bool loadNextBall = false;
+    bool onFirstAttempt = false;
+
+    private void Start()
     {
-        
+        remainingBallCount = balls.Length;
+        remainingBallText.text = remainingBallCount.ToString();
+        onFirstAttempt = true;
+        LoadNextBall();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MoveBallThrower();
+    }
+
+    void LoadNextBall()
+    {
+
+        if (onFirstAttempt)
+        {
+            balls[ballPoolIndex].transform.SetParent(ballThrower.transform);
+            balls[ballPoolIndex].transform.position = ballSpawnPosition.transform.position;
+            balls[ballPoolIndex].SetActive(true);
+
+            chosenBall = balls[ballPoolIndex];
+
+            ballPoolIndex++;
+
+            balls[ballPoolIndex].transform.position = nextBallPosition.transform.position;
+            balls[ballPoolIndex].SetActive(true);
+
+            remainingBallText.text = remainingBallCount.ToString();
+            onFirstAttempt = false;
+        }
+        else
+        {
+            if (balls.Length != 0)
+            {
+                balls[ballPoolIndex].transform.SetParent(ballThrower.transform);
+                //balls[ballPoolIndex].transform.position = ballSpawnPosition.transform.position;
+                balls[ballPoolIndex].transform.position = Vector3.Lerp(nextBallPosition.transform.position, ballSpawnPosition.transform.position, 450 * Time.deltaTime);
+                balls[ballPoolIndex].SetActive(true);
+
+                chosenBall = balls[ballPoolIndex];
+
+                remainingBallCount--;
+                remainingBallText.text = remainingBallCount.ToString();
+
+                if (ballPoolIndex == balls.Length - 1)
+                {
+                    Debug.Log("Gameover");
+                }
+                else
+                {
+                    ballPoolIndex++;
+                    balls[ballPoolIndex].transform.position = nextBallPosition.transform.position;
+                    balls[ballPoolIndex].SetActive(true);
+                }
+            }
+        }
+
+
+    }
+
+    private void MoveBallThrower()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.CompareTag("Ground"))
+                {
+                    Vector2 myMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                    ballThrower.transform.position = Vector2.MoveTowards(ballThrower.transform.position, new Vector2(myMousePosition.x, ballThrower.transform.position.y), 30 * Time.deltaTime);
+                }
+            }
+
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            chosenBall.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            chosenBall.transform.SetParent(null);
+            chosenBall.GetComponent<Ball>().ChangePrimeryState();
+            loadNextBall = true;
+        }
+
+        if (loadNextBall)
+        {
+            loadNextBall = false;
+            Invoke("LoadNextBall", .3f);
+        }
     }
 }
